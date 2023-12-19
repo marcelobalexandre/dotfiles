06a40f01b4ca7ac3284ee6171d350885ade873d5
Marcelo Alexandre <marcelobalexandre@gmail.com>
2023-12-19 17:17:43 -0300

    Add fugitive keymaps

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gd', vim.cmd.Gdiffsplit)
+vim.keymap.set('n', '<leader>gl', function() vim.cmd 'Git log' end)
+vim.keymap.set('n', '<leader>gc', function() vim.cmd 'Git commit' end)
