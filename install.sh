02177c3322409d3a8c9ec4f60089303a95881aa3
Marcelo Alexandre <marcelobalexandre@gmail.com>
2023-12-19 16:54:27 -0300

    Add support to macOS

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-find -maxdepth 1 -type d ! -name . ! -name .git -printf '%f\n' | xargs -n1 stow --target=$HOME --restow --verbose
+find="find"
+xargs="xargs"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find="gfind"
+  xargs="gxargs"
+fi
+
+eval "$find . -maxdepth 1 -type d ! -name . ! -name .git -printf '%f\n'" | eval "$xargs -n1 stow --target=$HOME --restow --verbose"
