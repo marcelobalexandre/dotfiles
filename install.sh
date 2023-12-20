7c2087fbb44ea7277f03ec2e43eeadf31338a0c3
Marcelo Alexandre <marcelobalexandre@gmail.com>
2023-12-20 17:14:19 -0300

    Add macOS KeyBindings

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  gfind . -maxdepth 1 -type d ! -name . ! -name .git -printf '%f\n' | gxargs -n1 stow --target=$HOME --restow --verbose
+  echo "Installing .files for macOS..."
+  gfind . -maxdepth 1 -type d ! -name . ! -name .git ! -name macos -printf '%f\n' | gxargs -n1 stow --target=$HOME --restow --verbose
+  cd macos
+  gfind . -maxdepth 1 -type d ! -name . -printf '%f\n' | gxargs -n1 stow --target=$HOME --restow --verbose
+  echo "Done."
   exit
 fi
 
-find . -maxdepth 1 -type d ! -name . ! -name .git ! -name yabai ! -name skhd -printf '%f\n' | xargs -n1 stow --target=$HOME --restow --verbose
+echo "Installing .files..."
+find . -maxdepth 1 -type d ! -name . ! -name .git ! -name macos -printf '%f\n' | xargs -n1 stow --target=$HOME --restow --verbose
+echo "Done."
