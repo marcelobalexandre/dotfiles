b26e088eecb8ea454494bd4e85aab86f2ac274dc
Marcelo Alexandre <marcelobalexandre@gmail.com>
2024-02-16 10:26:21 -0300

    Move i3 to linux folder

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Installing dotfiles for macOS..."
-  gfind . -maxdepth 1 -type d ! -name . ! -name .git ! -name macos -printf '%f\n' | gxargs -n1 stow --target=$HOME --restow --verbose
+  gfind . -maxdepth 1 -type d ! -name . ! -name .git ! -name linux ! -name macos -printf '%f\n' | gxargs -n1 stow --target=$HOME --restow --verbose
   cd macos
   gfind . -maxdepth 1 -type d ! -name . -printf '%f\n' | gxargs -n1 stow --target=$HOME --restow --verbose
   echo "Done."
@@ -10,5 +10,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 echo "Installing dotfiles..."
-find . -maxdepth 1 -type d ! -name . ! -name .git ! -name macos -printf '%f\n' | xargs -n1 stow --target=$HOME --restow --verbose
+find . -maxdepth 1 -type d ! -name . ! -name .git ! -name linux ! -name macos -printf '%f\n' | xargs -n1 stow --target=$HOME --restow --verbose
+cd linux
+
+find . -maxdepth 1 -type d ! -name . ! -name .git -printf '%f\n' | xargs -n1 stow --target=$HOME --restow --verbose
 echo "Done."
